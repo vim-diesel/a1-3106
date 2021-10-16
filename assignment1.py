13f8ef105eda09f54992baab0d1b8bef795f1344
Ian <37084860+vim-diesel@users.noreply.github.com>
2021-10-16 15:56:48 +0000

    added nodes

--- assignment1.py
+++ assignment1.py
@@ -1,17 +1,27 @@
 # Name this file to assignment1.py when you submit
 
+class node:
+    def __init__(self, x, y, parent=None, action=None, path_cost=0):
+        self.x = x
+        self.y = y
 
 def pathfinding(input_filepath):
     # input_filepath contains the full path to a CSV file with the input grid
-    with open(input_filepath, "r") as inputs:
+    list = []
+    count_x = 0
+    count_y = 0
 
+    with open(input_filepath, "r") as inputs:
         tmp = inputs.read().split("\n")
         tmp.remove('')
         array = [i.split(",") for i in tmp]
+
         for row in array:
             for item in row:
-                print(item, end=" ")
-            print("")
+                list.append( node(count_x, count_y))
+                count_x += 1
+            count_y += 1
+            count_x = 0
 
     # optimal_path is a list of tuples indicated the optimal path from start to goal
     # explored_list is the list of nodes explored during search
