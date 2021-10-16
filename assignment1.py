bec66e6193a2ee30fbc261b993c82e28935bca8a
Ian <37084860+vim-diesel@users.noreply.github.com>
2021-10-16 17:57:40 +0000

    nodes

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,7 @@
 # Name this file to assignment1.py when you submit
 
 class node:
-    def __init__(self, x, y, label, parent=None, action=None, path_cost=0, isStart=0, isGoal=0, isObstacle=0, isHazard=0):
+    def __init__(self, x, y, label, parent=None, action=None, path_cost=0):
         self.x = x
         self.y = y
         self.label = label
@@ -11,6 +11,7 @@ def pathfinding(input_filepath):
     list = []
     count_x = 0
     count_y = 0
+    
 
     with open(input_filepath, "r") as inputs:
         tmp = inputs.read().split("\n")
@@ -19,18 +20,6 @@ def pathfinding(input_filepath):
 
         for row in array:
             for item in row:
-                """""
-                if(item == "S"):
-                    list.append( node(count_x, count_y, isStart=1))
-                elif(item == "G"):
-                    list.append( node(count_x, count_y, isGoal=1))
-                elif(item == "H"):
-                    list.append( node(count_x, count_y, isHazard=1))
-                elif(item == "X"):
-                    list.append( node(count_x, count_y, isObstacle=1))
-                else:
-                    list.append( node(count_x, count_y))
-                """
                 list.append( node(count_x, count_y, item))
                 count_x += 1
             count_y += 1
@@ -38,6 +27,8 @@ def pathfinding(input_filepath):
 
         for obj in list:
             print(obj.x, obj.y, obj.label, sep=" ")
+            if(obj.label == "S"):
+                
 
     # optimal_path is a list of tuples indicated the optimal path from start to goal
     # explored_list is the list of nodes explored during search
